ede6bf76879a78aa370704ca7a7cbfa7e30b3efd
MISSIC17 <prajjwalt9@gmail.com>
2025-07-08 16:04:33 +0545

    feat: remove value from select to display placeholder

--- src/components/custom/theme-changer.tsx
+++ src/components/custom/theme-changer.tsx
@@ -5,30 +5,24 @@ import { useTheme } from "next-themes";
 import {
   Select,
   SelectContent,
-  SelectGroup,
   SelectItem,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
 export const ThemeChanger = () => {
-  const { theme, setTheme } = useTheme();
-  React.useEffect(() => {
-    if (!theme) setTheme("dark");
-  }, []);
+  const { setTheme } = useTheme();
 
   return (
-    <Select value={theme || "dark"} onValueChange={(value) => setTheme(value)}>
-      <SelectTrigger className="w-[100px] lg:w-[180px] border-foreground/70 text-foreground">
+    <Select onValueChange={(value) => setTheme(value)}>
+      <SelectTrigger className="w-[100px] lg:w-[180px] border-foreground/70 text-foreground dark:text-foreground">
         <SelectValue
           placeholder="Select theme"
-          className="!text-foreground dark:!text-white"
+          className="!text-foreground/80 dark:!text-white/80"
         />
       </SelectTrigger>
       <SelectContent>
-        <SelectGroup>
-          <SelectItem value="light">Light</SelectItem>
-          <SelectItem value="dark">Dark</SelectItem>
-        </SelectGroup>
+        <SelectItem value="light">Light</SelectItem>
+        <SelectItem value="dark">Dark</SelectItem>
       </SelectContent>
     </Select>
   );
