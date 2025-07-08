e70eda67756b1552463d5317c6816392cbf97cae
MISSIC17 <prajjwalt9@gmail.com>
2025-07-08 15:39:04 +0545

    feat: add default theme for theme switcher

--- src/components/custom/theme-changer.tsx
+++ src/components/custom/theme-changer.tsx
@@ -14,7 +14,7 @@ export const ThemeChanger = () => {
   const { theme, setTheme } = useTheme();
 
   return (
-    <Select value={theme} onValueChange={(value) => setTheme(value)}>
+    <Select value={theme || "dark"} onValueChange={(value) => setTheme(value)}>
       <SelectTrigger className="w-[100px] lg:w-[180px] border-foreground/70 text-foreground">
         <SelectValue
           placeholder="Select theme"
